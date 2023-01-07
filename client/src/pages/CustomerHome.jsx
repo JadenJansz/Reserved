9b54dc12310137aae41beee32bafe79708752f6f
Rukith Ranasinghe <rukith.ranasinghe@gmail.com>
2023-01-07 11:25:15 +0530

    Updated UI

--- client/src/pages/CustomerHome.jsx
+++ client/src/pages/CustomerHome.jsx
@@ -10,14 +10,15 @@ const CustomerHome = () => {
       <NavBar />
       <Search />
       <Popular />
+      <Join />
 
-<Iframe url="https://www.google.com/maps/place/LiveRoom+(Pvt)+Ltd/@6.8730458,79.9017449,15z/data=!4m5!3m4!1s0x0:0xb3bb47840c126875!8m2!3d6.8730462!4d79.9017447"
+{/* <Iframe url="https://www.google.com/maps/place/LiveRoom+(Pvt)+Ltd/@6.8730458,79.9017449,15z/data=!4m5!3m4!1s0x0:0xb3bb47840c126875!8m2!3d6.8730462!4d79.9017447"
         width="640px"
         height="320px"
         id=""
         className=""
         display="block"
-        position="relative"/>
+        position="relative"/> */}
     </div>
   );
 };
